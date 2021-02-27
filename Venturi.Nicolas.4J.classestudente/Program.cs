b02af14991311a04706b9673007075fe817d97d8
NikyDev <66072452+NikyDev@users.noreply.github.com>
2021-02-27 10:17:11 +0100

    primo commit

---
+++ Venturi.Nicolas.4J.classestudente/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Venturi.Nicolas._4J.classestudente.models;
+using System.IO;
+
+namespace Venturi.Nicolas._4J.classestudente
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter = 0, i = 0;
+            string ricerca, outo="";
+            using (StreamReader file = new StreamReader("cognomi.txt"))
+            {
+                
+                string linea;
+                while((linea=file.ReadLine())!=null)
+                {
+                    counter++;
+                }
+                Studente[] name = new Studente[counter];
+                file.BaseStream.Position = 0;
+                while ((linea = file.ReadLine()) != null)
+                {
+                     name[i] = new Studente(linea, 0);
+                    i++;
+                }
+            
+
+            Console.WriteLine("inserisci il cognome dello studente a cui vuoi aggiungere una assenza");
+            ricerca = Console.ReadLine();
+                i = 0;
+            while (i<counter)
+            {
+                    if (ricerca == name[i].cognome)
+                    {
+                      name[i].assenze=name[i].aggiungiassenze(name[i].cognome, name[i].assenze);
+                    }
+                    i++;
+            }
+                Console.WriteLine("inserisci il cognome dello studente di cui vuoi sapere le assenze");
+                ricerca = Console.ReadLine();
+                i = 0;
+                while (i < counter)
+                {
+                    if (ricerca == name[i].cognome)
+                    {
+                       outo =  name[i].ToString();
+                    }
+                    i++;
+                }
+                Console.WriteLine(outo);
+                Console.ReadLine();
+            }
+           
+        }
+    }
+}
---
+++ Venturi.Nicolas.4J.classestudente/models/studente.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Venturi.Nicolas._4J.classestudente.models
+{
+    class Studente
+    {
+        private string _cognome;
+        private int _assenze;
+
+        public string cognome
+        {
+            get
+            {
+                return _cognome;
+            }
+
+            set
+            {
+                _cognome = value;
+            }
+        }
+
+        public int assenze
+        {
+            get
+            {
+                return _assenze;
+            }
+
+            set
+            {
+                _assenze = value;
+            }
+        }
+
+        public  Studente(string s, int a)
+        {
+            cognome = s;
+            assenze = 0;
+        }
+
+        public int aggiungiassenze(string s, int a)
+        {
+            int asse = a;
+            asse++;
+            return asse;
+        }
+
+        public override string ToString()
+        {
+            string linea;
+            linea = "lo studente " + cognome + " ha collezionato " + assenze + " assenze";
+            return linea;
+        }
+    }
+}
